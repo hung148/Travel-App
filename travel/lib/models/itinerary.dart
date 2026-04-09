c28448ca57e6b35a9e5f83e1056c69053deec903
Trinh Dinh Nguyen Hung <trinhhungqt2004@gmail.com>
2026-04-09 11:06:15 -0700

    add itinerary model

---
+++ travel/lib/models/itinerary.dart
@@ -0,0 +1,35 @@
+class Itinerary {
+  final String id;
+  final int dayNumber;
+  final String places;
+  final String meals;
+  final double estimatedCost;
+
+  Itinerary({
+    required this.id,
+    required this.dayNumber,
+    required this.places,
+    required this.meals,
+    required this.estimatedCost,
+  });
+
+  factory Itinerary.fromMap(Map<String, dynamic> data, String documentId) {
+    return Itinerary(
+      id: data['id'], 
+      dayNumber: data['dayNumber'], 
+      places: data['places'], 
+      meals: data['meals'], 
+      estimatedCost: data['estimatedCost'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'dayNumber': dayNumber,
+      'places': places,
+      'meals': meals,
+      'estimatedCost': estimatedCost, 
+    };
+  }
+}
