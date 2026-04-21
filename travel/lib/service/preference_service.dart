caf0569732e1c046177e9a6307aca55094e915b2
Trinh Dinh Nguyen Hung <trinhhungqt2004@gmail.com>
2026-04-21 11:40:27 -0700

    add config and debug flag, change preference service to return fake data when in debug mode and test preference screen

--- travel/lib/service/preference_service.dart
+++ travel/lib/service/preference_service.dart
@@ -1,14 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:travel/config/app_config.dart';
 import 'package:travel/models/preference/preference_result.dart';
 import 'package:travel/models/preference/preferences.dart';
 
 class PreferenceService {
   final CollectionReference preferenceRef = FirebaseFirestore.instance.collection('preferences');
 
+    // Fake data
+    static final Preference _fakePreference = Preference(
+      id: 'debug-preference-id',
+      ownerId: 'test-user',
+      experienceType: 'nature',
+      activityLevel: 'hyper active',
+      spendingStyle: 'I pay for the whole trip',
+      interests: ['exploring', 'coffee'],
+    );
+
   /// CRUD
   
   // CREATE - add a new preference
   Future<PreferenceResult> addPreference(Preference preference) async {
+    if (AppConfig.isDebug) {
+      return PreferenceResult(success: true, data: null, error: "");
+    }
+
     try {
       await preferenceRef.doc(preference.id).set(preference.toMap());
       return PreferenceResult(success: true, data: null, error: "");
@@ -19,6 +34,10 @@ class PreferenceService {
 
   // READ - by user id
   Future<PreferenceResult> getPreferences(String ownerId) async {
+    if (AppConfig.isDebug) {
+      return PreferenceResult(success: true, data: _fakePreference, error: "");
+    }
+
     try {
       final snapshot = await preferenceRef
         .where('ownerId', isEqualTo: ownerId)
